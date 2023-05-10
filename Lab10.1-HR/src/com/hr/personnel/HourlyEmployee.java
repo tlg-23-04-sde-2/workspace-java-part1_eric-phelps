b01b1f1bdb834a43632832bd2eb8bbf1bfa12b90
Eric Phelps <Eric.Phelps@tlgcohort.com>
2023-05-10 18:05:34 -0400

    Lab10.2 complete

--- Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
@@ -8,22 +8,31 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
-public class HourlyEmployee extends Employee {
+public class HourlyEmployee extends Employee
+{
+    //static fields
+    public static final double FEDERAL_MINIMUM_WAGE = 15.0;
+
     // fields
     private double rate;
     private double hours;
 
     // constructors
-    public HourlyEmployee() {
+    public HourlyEmployee()
+    {
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate) {
+    public HourlyEmployee(String name, LocalDate hireDate)
+    {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) throws IllegalWageException
+    {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -31,35 +40,50 @@ public class HourlyEmployee extends Employee {
 
     // business methods
     @Override
-    public void pay() {
+    public void pay()
+    {
         System.out.println(getName() + " is paid hourly " + (getRate() * getHours()));
     }
 
     @Override  // interface TaxPayer
-    public void payTaxes() {
+    public void payTaxes()
+    {
         double taxes = getRate() * getHours() * HOURLY_TAX_RATE;
         System.out.println(getName() + " paid taxes of " + taxes);
     }
 
     // accessor methods
-    public double getRate() {
+    public double getRate()
+    {
         return rate;
     }
 
-    public void setRate(double rate) {
-        this.rate = rate;
+    public void setRate(double rate)  throws IllegalWageException
+    {
+        if (rate >= FEDERAL_MINIMUM_WAGE)
+        {
+            this.rate = rate;
+        }
+        else
+        {
+            throw new IllegalWageException(String.format("The federal minimum wage is: %s, you must pay at least this rate if not higher", FEDERAL_MINIMUM_WAGE));
+        }
+
     }
 
-    public double getHours() {
+    public double getHours()
+    {
         return hours;
     }
 
-    public void setHours(double hours) {
+    public void setHours(double hours)
+    {
         this.hours = hours;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return super.toString() + ", rate=" + getRate() + ", hours=" + getHours();
     }
 }
